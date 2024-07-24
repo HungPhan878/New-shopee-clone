8a561c509b5c6b7fe7e5d7e5140ddaad3a4baba1
Phan Van Hung <Hungphanhung8@gmail.com>
2024-07-24 22:07:17 +0700

    Handle message when err !== 422

--- src/components/utils/http.ts
+++ src/components/utils/http.ts
@@ -1,4 +1,9 @@
+/* eslint-disable import/no-unresolved */
 import axios, { type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
+
+// components
+import HttpStatusCode from '@/constants/httpStatusCode.enum'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +15,23 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+
+    // Add a response interceptor
+    this.instance.interceptors.response.use(
+      function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response
+      },
+      function (error) {
+        if (!(error?.response?.status === HttpStatusCode.UnprocessableEntity)) {
+          const data = error.response.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
