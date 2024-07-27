c38ef3a45698f1bb6e12052431ec496febdb05c7
Phan Van Hung <Hungphanhung8@gmail.com>
2024-07-27 21:57:04 +0700

    Setting rejected and protected route

--- src/hooks/useRouteElements.tsx
+++ src/hooks/useRouteElements.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable import/no-unresolved */
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 
 // Components
 import RegisterLayout from '@/layouts/RegisterLayout'
@@ -7,24 +8,55 @@ import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import MainLayout from '@/layouts/MainLayout'
 import ProductList from '@/pages/ProductList'
+import Profile from '@/pages/Profile'
+
+const isAuthentication = false
+
+const ProtectedRoute: () => JSX.Element = () => {
+  return isAuthentication ? <Outlet /> : <Navigate to='/login' />
+}
+
+const RejectedRoute: () => JSX.Element = () => {
+  return !isAuthentication ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '/',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '/',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     },
     {
       path: '/',
