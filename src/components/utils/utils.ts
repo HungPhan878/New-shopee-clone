82f26b3715c5b5d88c216bce75bea3f524e4e30f
Phan Van Hung <Hungphanhung8@gmail.com>
2024-08-07 21:41:34 +0700

    Show product and formatNumber and custom hook useQueryParams

--- src/components/utils/utils.ts
+++ src/components/utils/utils.ts
@@ -16,3 +16,17 @@ export function isUnprocessableEntityError<FormError>(
   return isAxiosError(error) && error?.response?.status === HttpStatusCode.UnprocessableEntity
 }
 //Note: AxiosError<Data> sẽ truyền vào data trong res err của axios
+
+export function formatCurrency(currency: number) {
+  return new Intl.NumberFormat('de-DE').format(currency)
+}
+
+export function formatNumberToSocialStyle(value: number) {
+  return new Intl.NumberFormat('en', {
+    notation: 'compact',
+    maximumFractionDigits: 1
+  })
+    .format(value)
+    .replace('.', ',')
+    .toLowerCase()
+}
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable import/no-unresolved */
+import { useQuery } from '@tanstack/react-query'
+
+// components
 import AsideFilter from './Components/AsideFilter'
 import Product from './Components/Product'
 import SortProductList from './Components/SortProductList'
+import productApi from '@/apis/product.api'
+import useQueryParams from '@/hooks/useQueryParams'
 
 export default function ProductList() {
+  const queryParams = useQueryParams()
+  const getProductList = useQuery({
+    queryKey: ['products', queryParams],
+    queryFn: () => productApi.getProducts(queryParams)
+  })
+  const products = getProductList.data?.data?.data.products
+
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -13,13 +26,11 @@ export default function ProductList() {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='col-span-1' key={index}>
-                    <Product />
-                  </div>
-                ))}
+              {products?.map((product) => (
+                <div className='col-span-1' key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
